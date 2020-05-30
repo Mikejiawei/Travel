8cba2c4db8307f9308f10b0a8eae35bc39caa9b8
Mikejiawei <15270013718@163.com>
2020-05-30 17:06:15 +0800

    add swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true
+      },
+      swiperList: [
+        {
+          id: "0001",
+          imgUrl:
+            "https://images.pexels.com/photos/1430677/pexels-photo-1430677.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "https://images.pexels.com/photos/386025/pexels-photo-386025.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullets
+  bottom 20px
+  .swiper-pagination-bullet-active
+    background #fff
+.wrapper
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom 30%
+  background #eee
+  .swiper-img
+    height 2.5rem
+    width 100%
+</style>
