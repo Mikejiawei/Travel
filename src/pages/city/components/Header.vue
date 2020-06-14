6df28054c38cdf9accc884b76fe23ba855f3ac61
Mikejiawei <15270013718@163.com>
2020-06-14 22:57:29 +0800

    add city-router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe659;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityHeader"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variable.styl';
+
+.header {
+  overflow: hidden;
+  height: 0.86rem;
+  line-height: 0.86rem;
+  text-align: center;
+  color: #ffffff;
+  font-size: 0.32rem;
+  background: $bgColor;
+
+  .header-back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0.64rem;
+    text-align: center;
+    font-size: 0.4rem;
+    color: #fff;
+  }
+}
+</style>
