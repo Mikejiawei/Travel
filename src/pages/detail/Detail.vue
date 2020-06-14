9e949dcde784be3eb82eaf5d3b91137b27d3686e
Mikejiawei <15270013718@163.com>
2020-06-14 22:24:50 +0800

    add recommend

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>详情</div>
+</template>
+
+<script>
+export default {
+  name: "Detail"
+};
+</script>
+
+<style lang="stylus" scoped></style>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,8 +2,8 @@
   <div>
     <home-header></home-header>
     <home-swiper :list="swiperList"></home-swiper>
-
     <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
   </div>
 </template>
 
@@ -11,6 +11,7 @@
 import HomeHeader from "./components/Header";
 import HomeSwiper from "./components/Swiper";
 import HomeIcons from "./components/Icons";
+import HomeRecommend from "./components/Recommend";
 import axios from "axios";
 import { mapState } from "vuex";
 export default {
@@ -18,7 +19,8 @@ export default {
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend
   },
   data() {
     return {
