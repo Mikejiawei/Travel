f9b37403a0ba83a1779441eec963b6d0e453b1c6
Mikejiawei <15270013718@163.com>
2020-06-14 22:33:28 +0800

    add weekend board

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,7 @@
     <home-swiper :list="swiperList"></home-swiper>
     <home-icons :list="iconList"></home-icons>
     <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import HomeHeader from "./components/Header";
 import HomeSwiper from "./components/Swiper";
 import HomeIcons from "./components/Icons";
 import HomeRecommend from "./components/Recommend";
+import HomeWeekend from "./components/Weekend";
 import axios from "axios";
 import { mapState } from "vuex";
 export default {
@@ -20,7 +22,8 @@ export default {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeRecommend
+    HomeRecommend,
+    HomeWeekend
   },
   data() {
     return {
